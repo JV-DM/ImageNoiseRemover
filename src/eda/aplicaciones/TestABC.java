0916d100713e57e9b7b6718569dc650b6a154442
daiant <cmg2512@gmail.com>
2019-05-06 19:38:43 +0200

    Ahora se reconstruye equilibrado creo

--- src/eda/aplicaciones/TestABC.java
+++ src/eda/aplicaciones/TestABC.java
@@ -1,32 +1,28 @@
 package eda.aplicaciones;
-import  eda.librerias.estructurasDeDatos.jerarquicos.ABC;
+import  eda.librerias.estructurasDeDatos.jerarquicos.ABB;
 import java.util.Random;
 
 public class TestABC {
     public static void main(String[] args) {
-	ABC<Integer> arbre = new ABC();
+	ABB<Integer> arbre = new ABB();
 	Random gen = new Random();
 	
 	for (int i = 0; i < 11; i++) {
-	    arbre.inserir(i);
+	    arbre.insertar(i);
 	    //arbre.inserir(gen.nextInt(1000));
 	}
 	for (int i = -1; i > -11; i--) {
-	    arbre.inserir(i);
+	    arbre.insertar(i);
 	    //arbre.inserir(gen.nextInt(1000));
 	}
-	System.out.println(arbre.inOrdre());
-
 	//System.out.println(arbre.enRang(50,-75));
-	arbre.esborrar(0);
-	System.out.println(arbre.inOrdre());
-	ABC<Integer> a = new ABC<>();
-	a.inserir(1);
-	a.inserir(2);
-	a.inserir(3);
-	a.inserir(4);
-	a.inserir(0);
-	System.out.println(arbre.teDosFills());
+	arbre.eliminar(0);
+	ABB<Integer> a = new ABB<>();
+	a.insertar(1);
+
+	System.out.println(a.teDosFills());
+	a.reconstruirEquilibrado();
+	System.out.println(a.teDosFills());
     }
 
 }
